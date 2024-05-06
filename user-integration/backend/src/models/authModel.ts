b4020253b6b258b4cc621e7cb79238dbe537574c
DanielBrMz <a01254805@tec.mx>
2024-05-06 00:00:00 -0600

    routes

--- user-integration/backend/src/models/authModel.ts
+++ user-integration/backend/src/models/authModel.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import sql from "../config/db";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import authModel from "../models/authModel";
 
 const signup = async (req: Request, res: Response, next: NextFunction) => {
 
