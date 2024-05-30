c9787a4fe7e1a6461f88ec5fcc40d15a1c580db7
DanielBrMz <a01254805@tec.mx>
2024-05-30 00:00:00 -0600

    userModel created

---
+++ user-integration/backend/src/controllers/userControllers.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+import sql from "../config/db";
+import userModel from "../models/userModel";
+
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+
+};
+
+export default {  createUser };
---
+++ user-integration/backend/src/models/userModel.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+import sql from "../config/db";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+
+};
+
+
+export default { };
