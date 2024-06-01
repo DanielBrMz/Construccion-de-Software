48eecf0542959fa540e2cae9670d5c692f2133d1
DanielBrMz <a01254805@tec.mx>
2024-06-01 00:00:00 -0600

    SelectItem

--- user-integration/frontend/src/components/ui/select.tsx
+++ user-integration/frontend/src/components/ui/select.tsx
@@ -1,14 +1,6 @@
-import React, { ForwardedRef } from 'react';
 import * as Select from '@radix-ui/react-select';
-import classnames from 'classnames';
-import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
-
-interface SelectItemProps {
-  children: React.ReactNode;
-  className?: string;
-  value: string;
-  disabled?: boolean;
-}
+import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
+import SelectItem from './selecitem';
 
 const SelectDemo = () => (
   <Select.Root>
@@ -73,22 +65,6 @@ const SelectDemo = () => (
   </Select.Root>
 );
 
-const SelectItem = React.forwardRef(({ children, className, ...props }: SelectItemProps, forwardedRef: ForwardedRef<HTMLDivElement>) => {
-  return (
-    <Select.Item
-      className={classnames(
-        'text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1',
-        className
-      )}
-      {...props}
-      ref={forwardedRef}
-    >
-      <Select.ItemText>{children}</Select.ItemText>
-      <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
-        <CheckIcon />
-      </Select.ItemIndicator>
-    </Select.Item>
-  );
-});
+
 
 export default SelectDemo;
