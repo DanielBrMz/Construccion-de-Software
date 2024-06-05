ee843a79c27c10c487ab99e2ffe908fdb8160d03
DanielBrMz <a01254805@tec.mx>
2024-06-04 21:04:24 -0600

    algo bonito

--- Integration/views/dashboard/Dashboard.jsx
+++ Integration/views/dashboard/Dashboard.jsx
@@ -1,33 +1,36 @@
-import Card from '../dashboard/components/card';
-import { useEffect, useState } from 'react';
+import Card from "../dashboard/components/card";
+import { useEffect, useState } from "react";
 
-function Dashboard() {
+// eslint-disable-next-line react/prop-types
+function Dashboard({name}) {
   const [users, setUsers] = useState([]);
 
   const fetchUsers = async () => {
-      try {
-          const response = await fetch('http://localhost:3000/users');
-          const data = await response.json();
-          setUsers(data);
-      } catch (error) {
-          console.error('Failed to fetch users:', error);
-      }
+    try {
+      const response = await fetch("http://localhost:3000/users");
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error("Failed to fetch users:", error);
+    }
   };
 
   useEffect(() => {
-      fetchUsers();
+    fetchUsers();
   }, []);
 
   return (
       <div>
-          <h1>Dashboard</h1>
-          <div>
-              {users.map(user => (
-                  <div key={user.id}>
-                      <Card user={user} />
-                  </div>
-              ))}
-          </div>
+        <h1>Dashboard</h1>
+        <div>
+          {users.map((user) => {
+              if (name && !user.name.includes(name)) return null;
+                return (<div key={user.id}>
+                    <Card user={user} />
+                  </div>)
+            }
+          )}
+        </div>
       </div>
   );
 }
