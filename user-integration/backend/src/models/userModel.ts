879500ecba9313e0484cfb05b03cf6e65b84408b
DanielBrMz <a01254805@tec.mx>
2024-06-06 00:00:00 -0600

    createUser

--- user-integration/backend/src/models/userModel.ts
+++ user-integration/backend/src/models/userModel.ts
@@ -1,13 +1,19 @@
-import supabase from '../config/db';
+import supabase from "../config/db";
+import { User } from "../types/types";
 
 const getAllUsers = async () => {
-  const { data } = await supabase.from('users').select()
+  const { data } = await supabase.from("users").select();
   return data;
-}
+};
 
 const getUserById = async (id: string) => {
-  const { data } = await supabase.from('users').select().eq('id', id)
+  const { data } = await supabase.from("users").select().eq("id", id);
   return data;
-}
+};
 
-export default { getAllUsers, getUserById };
+const createUser = async (user: User) => {
+  const { data } = await supabase.from("users").insert([user]);
+  return data;
+};
+
+export default { getAllUsers, getUserById, createUser };
