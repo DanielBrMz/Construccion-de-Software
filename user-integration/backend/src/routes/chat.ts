d748939ac0796eeb5913a01d1a37ecf35f755893
DanielBrMz <a01254805@tec.mx>
2024-06-06 00:00:00 -0600

    done, waiting for frontend

--- user-integration/backend/src/routes/chat.ts
+++ user-integration/backend/src/routes/chat.ts
@@ -1,7 +1,7 @@
 import express from "express";
+import chatController from "../controllers/chat";
 
 const chatRouter: express.Router = express.Router();
+chatRouter.post("/", chatController.getRagContext);
 
 export default chatRouter;
-
-chatRouter.post("/");
