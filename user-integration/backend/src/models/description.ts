eb9cba98317495f43f040491877dfcb0a059e96b
DanielBrMz <a01254805@tec.mx>
2024-06-06 00:00:00 -0600

    descriptions

--- user-integration/backend/src/models/description.ts
+++ user-integration/backend/src/models/description.ts
@@ -1,1 +1,25 @@
 import supabase from "../config/db";
+
+const getDescriptionByid = async (id: number) => {
+  const { data, error } = await supabase
+    .from("description")
+    .select("*")
+    .eq("id", id);
+  if (error) {
+    throw new Error(`Description could not be retrieved because of: ${error}`);
+  }
+  return data;
+};
+
+const createDescription = async (description: string, user_id: string) => {
+  const { data, error } = await supabase
+    .from("description")
+    .insert([{ description, user_id }]);
+  if (error) {
+    throw new Error(`Description could not be created because of: ${error}`);
+  }
+  return data;
+}
+
+
+export default { getDescriptionByid, createDescription };
--- user-integration/backend/src/routes/description.ts
+++ user-integration/backend/src/routes/description.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import descriptionController from '../controllers/description';
 
 const router = express.Router();
+
+router.get('/:id', descriptionController.getDescriptionByid);
+router.post('/:id', descriptionController.createDescription);
 
 export default router;
